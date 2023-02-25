f95d66d7338d67e0c75019e94b790ba3be9d33ee
jgonzalez-rsi <jgonzalez@rsimail.com>
2023-02-25 00:04:49 -0500

    Menu1 and Menu2 pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
+import Menu1 from "./pages/Menu1";
+import Menu2 from "./pages/Menu2";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />     
+        <Route path="/" element={<Home />} />  
+        <Route path="/menu" element={<Menu1 />} />   
+        <Route path="/menu2" element={<Menu2 />} />
       </Routes>
     </div>
   );
