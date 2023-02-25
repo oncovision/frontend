aef03267034aa59cddd992d5f6e6bb037a77f76e
cbiswal <125626248+cbiswal@users.noreply.github.com>
2023-02-25 11:45:02 -0600

    Update Page5.js

--- src/components/DataScientistTab/Page5.js
+++ src/components/DataScientistTab/Page5.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../../styles/LungPath.css";
 
 function Page5() {
-  return <div><iframe src="http://34.160.171.241/" width={1000} height={1000}></iframe></div>;
+  return <div><iframe src="http://10.61.129.233/" width={1000} height={1000}></iframe></div>;
  
 }
 
