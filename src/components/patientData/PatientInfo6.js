5208ea5f191267f2ac90a4601ff9d109237f24d1
cbiswal <125626248+cbiswal@users.noreply.github.com>
2023-02-25 11:45:35 -0600

    Update PatientInfo6.js

--- src/components/patientData/PatientInfo6.js
+++ src/components/patientData/PatientInfo6.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function PatientInfo6() {
-  return <div><iframe src="http://34.160.171.241/" width={1000} height={1000}></iframe></div>;
+  return <div><iframe src="http://10.61.129.233/" width={1000} height={1000}></iframe></div>;
 }
 
 export default PatientInfo6;
